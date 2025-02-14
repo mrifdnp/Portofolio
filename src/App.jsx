d20073d530bc5156552b4f2bfd46facc7317c988
Rifqi Dani Putranto <rifqidani23@gmail.com>
2025-02-14 12:49:43 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Technologies from './components/Technologies';
 import Experience from './components/Experience';
 import Project from './components/Project';
 import Contact from './components/Contact';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 
 
 const App = () => {
